a1dbf16a5bb5b0382d88013faa12348736018738
Dag <me@dag.im>
2014-12-13 12:15:02 +0100

    Add SIGINT and SIGTERM handlers to stop actor

--- cameraManager.py
+++ cameraManager.py
@@ -1,4 +1,7 @@
-from . import cameraActor.Camera
+from cameraActor import Camera
+import pykka
+import signal
+import sys
 
 camera = Camera.start()
 
@@ -11,4 +14,15 @@ def start_recording(params):
     camera.tell({'msg': 'START_RECORDING', 'params': params})
     
 def stop_recording():
-    camera.tell({'msg': 'STOP_RECORDING'})    
+    camera.tell({'msg': 'STOP_RECORDING'})
+    
+def sigterm_handler(_signo, _stack_frame):
+    
+    reg = pykka.ActorRegistry()
+    for act in reg.get_all():
+        act.stop(block=True)
+ 
+    sys.exit(0)
+ 
+signal.signal(signal.SIGINT, sigterm_handler)
+signal.signal(signal.SIGTERM, sigterm_handler)
