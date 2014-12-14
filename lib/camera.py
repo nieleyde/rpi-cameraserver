673c0035a7b8abab610179d3467d2930882e74c3
Dag <me@dag.im>
2014-12-14 01:23:58 +0100

    Reorganize code

--- lib/camera.py
+++ lib/camera.py
@@ -3,38 +3,45 @@ import signal
 import logging
 import time
 import io
-import picamera
 import sys
 import os
 from subprocess import Popen, PIPE
 
+from util import StreamToLogger 
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log = logging.getLogger('cameraserver.actor')
 
-logging.basicConfig(level=logging.DEBUG)
-class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
- 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
- 
-logging.basicConfig(
-   level=logging.DEBUG,
-   format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
-   filename="out.log",
-   filemode='a'
-)
 
-class Camera(pykka.ThreadingActor):
+class Camera():
     
-    logger = None
+    camera = None
+    
+    def __init__(self, picamera):
+        self.camera = CameraActor.start(picamera)
+        signal.signal(signal.SIGINT, self.sigterm_handler)
+        signal.signal(signal.SIGTERM, self.sigterm_handler)
+        
+    def take_picture(self, params = dict()):
+        picture = self.camera.ask({'msg': 'TAKE_PICTURE', 'params': params}) 
+        self.camera.tell({'msg': 'CONTINUE_RECORDING'})
+        return picture
+
+    def start_recording(self, params = dict()):    
+        self.camera.tell({'msg': 'START_RECORDING', 'params': params})
+        
+    def stop_recording(self):
+        self.camera.tell({'msg': 'STOP_RECORDING'})
+    
+    def stop(self):
+        self.camera.stop()
+        
+    def sigterm_handler(self, _signo, _stack_frame):
+        self.camera.stop()
+        sys.exit(0)
+
+
+class CameraActor(pykka.ThreadingActor):
+    
     is_recording = False
     
     process_psips = None
@@ -44,44 +51,39 @@ class Camera(pykka.ThreadingActor):
     camera = None
     
     
-    def on_start(self):
-        self.logger = logging.getLogger('cameraserver.actor')
-
-        
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
-        self.logger.debug('rpi-cameraserver: starting camera actor')
-        
-
-        self.picamera = picamera.PiCamera()
-        self.camera = self.picamera.__enter__()
+    def __init__(self, picamera):
+        super(pykka.ThreadingActor, self).__init__()
+        self.picamera = picamera
     
+    def on_start(self):
+        log.info('starting')
+        self.camera = self.picamera.__enter__()
+
     def on_stop(self):
-        self.picamera.__exit__(sys.exc_info())
-        
+        log.info('stopping')
+        self.stop_recording()
+        self.camera.__exit__(sys.exc_info())
     
     def on_receive(self, envelope):
         msg = envelope.get('msg')
         
         if(msg == 'TAKE_PICTURE'):
-            self.logger.info('rpi-cameraserver: taking a picture')
+            log.info('take picture')
             
             params = envelope.get('params')
             return self.take_picture(params)
             
         if(msg == 'START_RECORDING'):
-            self.logger.info('rpi-cameraserver: starting recording')
+            log.info('start recording')
             params = envelope.get('params')
             self.start_recording(True, params)
             
         if(msg == 'CONTINUE_RECORDING'):
-            self.logger.info('rpi-cameraserver: continuing recording %r' % self.is_recording)
+            log.info('continue recording')
             self.start_recording(False)
             
         if(msg == 'STOP_RECORDING'):
-            self.logger.info('rpi-cameraserver: stopping recording')
+            log.info('stop recording')
             self.stop_recording()
     
     
@@ -89,7 +91,8 @@ class Camera(pykka.ThreadingActor):
         if(init and not self.is_recording):
             self.is_recording = True
                         
-            self.camera.resolution = (int(params.get('width', 1280)), int(params.get('height', 720)))
+            self.camera.resolution = (int(params.get('width', 1280)), 
+                                      int(params.get('height', 720)))
             self.camera.framerate = int(params.get('framerate', 5))
             self.camera.rotation = int(params.get('rotation', 180))
             
@@ -97,30 +100,38 @@ class Camera(pykka.ThreadingActor):
             if(not os.path.exists("stream.h264")):
                 os.mkfifo("stream.h264")
             
+            log.info('starting psips process')
+            
             # start psips process to ensure we have SPS and PPS NALs in our h264 segments
             # psips will pipe its contents to a linux named pipe which is picked up by ffmpeg
-            self.process_psips = Popen(["psips"], 
+            self.process_psips = Popen(["psips"],
+                    shell=True,
                     stdin=PIPE,
                     stdout=open('stream.h264', 'w'), 
-                    stderr=StreamToLogger(logging.getLogger('psips'), logging.ERROR))
+                    stderr=None)
+            
+            log.info('starting recording')
             
             # pass h264 bits from pipicamera to psips stdin
-            self.camera.start_recording(psips.stdin, format='h264', bitrate=2000, quality=23)
+            self.camera.start_recording(self.process_psips.stdin, format='h264', bitrate=2000, quality=23)
+            
+            log.info('starting ffmpeg process')
             
             # start ffmpeg process
             self.start_ffmpeg()
-
-            # camera.start_recording
+            
         
+        log.info(" is_recording? %r  inbox empty? %r" % (self.is_recording, self.actor_inbox.empty()))
         while(self.is_recording and self.actor_inbox.empty()):
-            self.logger.info('rpi-cameraserver: recording')
+            log.info("recording")            
             self.camera.wait_recording(0.5)
             
     def start_ffmpeg(self):
         # let it buffer some video before we start ffmpeg
-        time.sleep(2)
+        time.sleep(.5)
         
         self.process_ffmpeg = Popen(["ffmpeg",
+            "-hide_banner", # disable version and build information in logging
             "-y",
             "-i", "stream.h264",
             "-c:v", "copy", # pass the video stream through without codecing
@@ -132,23 +143,30 @@ class Camera(pykka.ThreadingActor):
             "-segment_list", "stream/playlist.m3u8",
             "-segment_list_flags", "live",
             "stream/%08d.ts"
-            ], stderr=StreamToLogger(logging.getLogger('ffmpeg'), logging.ERROR)
-             , stdout=StreamToLogger(logging.getLogger('ffmpeg'), logging.INFO))
+            ], 
+            stderr=None, 
+            stdout=None)
     
     def stop_recording(self):
         self.is_recording = False
-        if(camera.recording):
-            self.camera.stop_recording()
-            self.process_psips.terminate()
+        
+        if(self.camera.recording):
+            
+            log.info('terminating ffmpeg')
             self.process_ffmpeg.terminate()
-            return "stopped recording"
-        return "was already stopped"         
+            self.process_ffmpeg.wait()
+            
+            log.info('stopping recording')
+            self.camera.stop_recording()
+            
+            log.info('terminating psips')
+            self.process_psips.terminate()
+            self.process_psips.wait()
 
-    
     def take_picture(self, params):
         
         camera = self.camera
-            
+        
         camera.rotation = params.get('rotation', 180)
         camera.resolution = (int(params.get('width', 1280)), int(params.get('height', 720)))
 
--- server.py
+++ server.py
@@ -2,31 +2,48 @@
 
 from flask import Flask, request, Response
 from flask import send_file, send_from_directory
-import cameraManager
+from picamera import Picamera
+from cameraserver.camera import Camera
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(name)s] %(levelname)s %(message)s')
 
-@app.route("/")
-def hello():
-    return "rpi-cameraserver!"
+try:
+    camera = Camera(PicameraMock())
 
-@app.route("/still")
-def still():
-    return Response(cameraManager.take_picture(request.args), mimetype='image/jpeg')
+    app = Flask(__name__)
 
-@app.route("/startstream")
-def start_stream():
-    cameraManager.start_recording(request.args)
-    return Response('{"message": "starting"}')
+    @app.route("/")
+    def hello():
+        return "rpi-cameraserver!"
 
-@app.route("/stopstream")
-def stop_stream():
-    cameraManager.stop_recording()
-    return Response('{"message": "stopping"}')
+    @app.route("/still")
+    def still():
+        return Response(camera.take_picture(request.args), mimetype='image/jpeg')
+
+    @app.route("/startstream")
+    def start_stream():
+        camera.start_recording(request.args)
+        return Response('{"message": "starting"}')
+
+    @app.route("/stopstream")
+    def stop_stream():
+        camera.stop_recording()
+        return Response('{"message": "stopping"}')
+        
+
+    @app.route("/stream/<path:filename>")
+    def serve_stream_data(filename):
+        return send_from_directory('stream', filename)
+
+    app.run(host='0.0.0.0')
     
+    
+    #camera.stop()
+    
+except KeyboardInterrupt:
+    print("stooooppp")
+    raise
 
-@app.route("/stream/<path:filename>")
-def serve_stream_data(filename):
-    return send_from_directory('stream', filename)
 
-app.run(host='0.0.0.0')
+except:
+    print "Caught it!"
